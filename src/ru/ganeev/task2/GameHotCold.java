a49a3edafa563835c9476c3eeebff5e688c7951f
AZAT <azyapshii@ya.ru>
2019-04-19 13:04:21 +0300

    Все задания по новой передел. проверка

--- src/ru/ganeev/task2/GameHotCold.java
+++ src/ru/ganeev/task2/GameHotCold.java
@@ -7,15 +7,6 @@ public class GameHotCold {
         Scanner s = new Scanner(System.in);
         int randomNum = (int) (Math.random()*10);
         System.out.println("Введите число");
-        while (s.hasNextInt()){
-            int n = s.nextInt();
-            if (n > randomNum)
-                System.out.println("Горячо");
-            else
-                System.out.println("Холодно");
-            if (n == randomNum)
-                System.out.println("Вы отгадали!");
-            System.out.println();
-        }
+        
     }
 }
