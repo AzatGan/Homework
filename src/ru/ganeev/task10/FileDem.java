0861b0fee66e5b6ae14439fbc03a089bcf853f41
AZAT <azyapshii@ya.ru>
2019-05-29 22:18:05 +0300

    Работа с файлами

--- src/ru/ganeev/task10/FileDem.java
+++ src/ru/ganeev/task10/FileDem.java
@@ -15,23 +15,39 @@ public class FileDem {
             System.out.println(e.getMessage());
         }
 
-        File fileorig1 = new File("..\\task10\\filetest.txt");
-        System.out.println(fileorig1.getAbsolutePath());
+//        File fileorig1 = new File("/task10/filetest.txt");
+//        System.out.println(fileorig1.getAbsolutePath());
 
-        File dir1 = new File("..\\Homework\\dir1");
+        File dir1 = new File("dir1");
         System.out.println("Создана директория " + " " + dir1.mkdir());
 
        // Files.createFile();
-        Path source = Paths.get("..\\task10\\filetest.txt");
-        Path destination = Paths.get("..\\dir1\\filetest.txt");
+        Path source = Paths.get(fileorig.getAbsolutePath());
+        Path destination = Paths.get("dir1/filetest.txt");
         try{
             Files.copy(source, destination);
-            System.out.println("file copied");
+            System.out.println("Копирование завершено");
         }catch (IOException i){
             System.out.println("Копирование не удалось");
         }
 
 
         System.out.println(fileorig.getName() + fileorig.delete());
+        recursiveDel(dir1);
+        System.out.println("Всё каталоги удалены");
+
+    }
+    private static void recursiveDel(File dir){
+        File[]files = dir.listFiles();
+        for (File file: files){
+            if(file.isFile()){
+                file.delete();
+                System.out.println("Файл <" + file + "> удален ");
+            }else {
+                recursiveDel(file);
+            }
+        }
+        dir.delete();
+        System.out.println("Каталог <" + dir + "> удалён");
     }
 }
